6c8e60e242e8146fac5761c8325f873f85f05be7
张浩刚 <iyunx@qq.com>
2020-03-18 13:51:29 +0000

    邮箱

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'PagesController@root')->name('root')->middleware(['verified']);
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
