77ff9ec92a2844a46bfcb883fd6f18184134f5a8
张浩刚 <iyunx@qq.com>
2020-03-18 13:02:30 +0000

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,5 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 function test_helper(){
     return 'ok';
+}
+
+//将请求的路由名，转换成css类名称
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
 }
